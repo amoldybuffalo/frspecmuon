c52e8a52e3b31c7bea7160bc17237f273fe994fd
Gregory Powers <gapowers27@wabash.edu>
2026-06-09 10:26:30 -0400

    Added some much needed comments

--- r_optimizer.py
+++ r_optimizer.py
@@ -2,6 +2,7 @@ import torch
 from torch.optim import Optimizer
 from riemann_layers import RiemannianLinear
 
+#stolen from the muon repo
 def adam_update(grad, buf1, buf2, step, betas, eps):
     buf1.lerp_(grad, 1 - betas[0])
     buf2.lerp_(grad.square(), 1 - betas[1])
@@ -9,13 +10,13 @@ def adam_update(grad, buf1, buf2, step, betas, eps):
     buf2c = buf2 / (1 - betas[1]**step)
     return buf1c / (buf2c.sqrt() + eps)
 
-
+#debugging function from earlier
 def assert_finite(name, x):
     if not torch.isfinite(x).all():
         raise RuntimeError(f"{name} became non-finite")
 
 class FrSpecMuon(Optimizer):
-    def __init__(self, model, lr=1e-2, beta = 0.2):
+    def __init__(self, model, lr=1e-2, beta = 0): #no momentum by default
 
 
         param_groups = []
@@ -24,6 +25,8 @@ class FrSpecMuon(Optimizer):
 
         remaining_params = []
         used_params = []
+
+        #decide whether the parameter gets a low rank muon update or a normal update based on whether it's a riemann layer or not (only riemannian linear layers for now)
         for module in flattened:
             if isinstance(module, RiemannianLinear):
                 param_groups.append(dict(params = [module.A, module.B], riemann = True, lr = lr))
@@ -40,7 +43,7 @@ class FrSpecMuon(Optimizer):
             assert "riemann" in group
             if group["riemann"]:
                 group["lr"] = group.get("lr", 0.02)
-                group["momentum"] = group.get("momentum", 0.95)
+                group["momentum"] = group.get("momentum", beta)
                 group["weight_decay"] = group.get("weight_decay", 0)
                 assert set(group.keys()) == set(["params", "lr", "momentum", "weight_decay", "riemann"])
                
@@ -60,8 +63,7 @@ class FrSpecMuon(Optimizer):
         self.r_tilde = [None] * riemann_param_count
         self.momentum = [None] * riemann_param_count
 
-        self.beta = beta
-        self.relaxation_tolerance = 0.95
+        self.relaxation_tolerance = 0.95 #recommended value 
         
 
     def right_multiply_by_Rinv(self, G, R):
@@ -72,13 +74,13 @@ class FrSpecMuon(Optimizer):
        
 
     def find_relaxation_coefficient(self, old_grad, new_grad, r, r_tilde, E, lr):
-        D = (1/lr) * (new_grad - old_grad).norm()**2  # scalar
-        a = ((r_tilde - E)**2).clamp(min=1e-5)          # [rank]
-        b = 2 * E * (r_tilde - E)                      # [rank]
-        c = E**2 - r_tilde**2 - (r_tilde - r)**2 - self.relaxation_tolerance * D  # [rank]
+        D = (1/lr) * (new_grad - old_grad).norm()**2 
+        a = ((r_tilde - E)**2).clamp(min=1e-5)          
+        b = 2 * E * (r_tilde - E)                      
+        c = E**2 - r_tilde**2 - (r_tilde - r)**2 - self.relaxation_tolerance * D 
 
-        discriminant = (b**2 - 4*a*c).clamp(min=0)    # guard sqrt domain
-        roots = (-b - discriminant.sqrt()) / (2 * a)   # [rank]
+        discriminant = (b**2 - 4*a*c).clamp(min=0)   
+        roots = (-b - discriminant.sqrt()) / (2 * a)   
         return roots.clamp(min=0)
 
     def tangent_core_svd(self, A, B):
@@ -87,12 +89,13 @@ class FrSpecMuon(Optimizer):
         """
 
         U, Rb = torch.linalg.qr(B, mode="reduced")
-        # V, Ra = torch.linalg.qr(A.T, mode="reduced")
-        # S = Rb * Ra
+
+        #A should alread be orthogonal so we don't do a Qr to save time
         V = A.T
         S = Rb
 
         # GV = self.right_multiply_by_Rinv(B.grad, Ra)
+        #Since Ra should just be the identity we don't bother with dividing by it
         GV = B.grad
         GTU = self.right_multiply_by_Rinv((A.grad).T, Rb)
 
@@ -109,6 +112,7 @@ class FrSpecMuon(Optimizer):
 
         r = U.shape[1]
 
+        # the small core
         C = torch.vstack([
             torch.hstack([K, RV.T]),
             torch.hstack([RU, torch.zeros(RU.shape[0], RV.shape[1],
@@ -129,7 +133,10 @@ class FrSpecMuon(Optimizer):
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
+                #I choose 1 as my kappa because it seems to work and choosing zero has lead to nan loss in the past (no idea why, the loss should never be negative)
                 E = torch.sqrt(loss + 1).item()
+
+                #some debugging steps from earlier
                 if torch.isnan(loss):
                     print("NAN LOSS")
                     print(self.r)
@@ -149,9 +156,10 @@ class FrSpecMuon(Optimizer):
 
                 C, S, U, V, QU, QV, rank = self.tangent_core_svd(A, B)  
                 
-
+                    
                 Uc, Sc, Vhc = torch.linalg.svd(C, full_matrices=False)
 
+                #truncate the core SVD
                 U_r = Uc[:, :rank]
                 S_r = Sc[:rank]
                 Vh_r = Vhc[:rank, :]
@@ -161,37 +169,131 @@ class FrSpecMuon(Optimizer):
                     self.r[k] = torch.full((rank,), E).to(device)
 
                 self.r_tilde[k] = self.evolve_discrete_energy(self.r[k], lr, S_r, E)
+
+                #  multiplying the gradient by the appropriate modified learning rate
                 step_sv = -(lr / E) * self.r_tilde[k]
-            
-
-                #Hk = -lr * C
-
-            
 
                 Hk = U_r @ torch.diag(step_sv) @ Vh_r  # [2r x 2r]
 
                 if self.momentum[k] is None:
                     self.momentum[k] = torch.zeros_like(C)
 
-                self.momentum[k] = self.beta * self.momentum[k] + Hk
+                # a clumsy attempt at implementing momentum into this (not using this right now)
+                self.momentum[k] = group["momentum"] * self.momentum[k] + Hk
 
                 S_pad = torch.zeros_like(Hk)
                 S_pad[:rank, :rank] = S  # current weight core
 
+                #I believe this is more or less the actual update step
                 Ak = S_pad + self.momentum[k]
 
+                # SVD back into the right basis
                 Ua, Sa, Vha = torch.linalg.svd(Ak, full_matrices=False)
 
+                # retract back
                 U_new = torch.cat([U, QU], dim=1) @ Ua[:, :rank]
                 S_new = torch.diag(Sa[:rank])
                 V_new = torch.cat([V, QV], dim=1) @ Vha[:rank, :].T
 
-                B.copy_(U_new @ S_new)
-                A.copy_(V_new.T)
-                X = B @ A
+                B.copy_(U_new @ S_new) # B gets the singular values 
+                A.copy_(V_new.T) # A remains orthogonal
+                X = B @ A 
 
-                zeta = self.find_relaxation_coefficient(old_X, X, self.r_tilde[k], self.r[k], E, lr)
+                # relaxation step
+                zeta = self.find_relaxation_coefficient(old_X, X, self.r_tilde[k], self.r[k], E, lr) 
                 self.r[k] = self.r_tilde[k] * zeta + (1 - zeta) * E 
+
+            else:
+                # otherwise just do a normal adam update as usual
+                for p in group["params"]:
+                    if p.grad is None:
+                        continue
+                    state = self.state[p]
+                    if len(state) == 0:
+                        state["exp_avg"] = torch.zeros_like(p)
+                        state["exp_avg_sq"] = torch.zeros_like(p)
+                        state["step"] = 0
+                    state["step"] += 1
+                    update = adam_update(p.grad, state["exp_avg"], state["exp_avg_sq"],
+                                         state["step"], group["betas"], group["eps"])
+                    p.mul_(1 - group["lr"] * group["weight_decay"])
+                    p.add_(update, alpha=-group["lr"])
+
+
+        return loss
+
+
+
+
+
+
+# Ignore this for now. Just an unfinished template to implement specmuon 
+class SpecMuon(Optimizer):
+    def __init__(self, param_groups, lr=1e-2, beta = 0.2):
+        for group in param_groups:
+            assert "use_muon" in group
+            if group["use_muon"]:
+                group["lr"] = group.get("lr", 0.02)
+                group["momentum"] = group.get("momentum", 0.95)
+                group["weight_decay"] = group.get("weight_decay", 0)
+                assert set(group.keys()) == set(["params", "lr", "momentum", "weight_decay", "use_muon"])
+               
+            else:
+                # defaults
+                group["lr"] = group.get("lr", 3e-4)
+                group["betas"] = group.get("betas", (0.9, 0.95))
+                group["eps"] = group.get("eps", 1e-10)
+                group["weight_decay"] = group.get("weight_decay", 0)
+                assert set(group.keys()) == set(["params", "lr", "betas", "eps", "weight_decay", "use_muon"])
+
+        
+        super().__init__(param_groups, dict())
+        
+        riemann_param_count =  len([param_group for param_group in param_groups if param_group["use_muon"]])
+        self.r = [None] * riemann_param_count
+        self.r_tilde = [None] * riemann_param_count
+        self.momentum = [None] * riemann_param_count
+
+        self.beta = beta
+        self.relaxation_tolerance = 0.95
+        
+
+
+    
+    def evolve_discrete_energy(self, r, lr, singular_values, energy):
+        return r / (1+ (lr/2)*(singular_values/energy**2))
+       
+
+    def find_relaxation_coefficient(self, old_grad, new_grad, r, r_tilde, E, lr):
+        D = (1/lr) * (new_grad - old_grad).norm()**2  
+        a = ((r_tilde - E)**2).clamp(min=1e-5)          
+        b = 2 * E * (r_tilde - E)                      
+        c = E**2 - r_tilde**2 - (r_tilde - r)**2 - self.relaxation_tolerance * D  
+
+        discriminant = (b**2 - 4*a*c).clamp(min=0)    # guard sqrt domain
+        roots = (-b - discriminant.sqrt()) / (2 * a)   
+        return roots.clamp(min=0)
+
+
+
+    @torch.no_grad()    
+    def step(self, closure=None):
+        loss = None
+        
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+                E = torch.sqrt(loss + 1).item()
+                if torch.isnan(loss):
+                    print("NAN LOSS")
+                    print(self.r)
+                    exit(-1)
+
+        k = -1 #I just wanna increment k first, okay?
+        for group in self.param_groups:
+            lr = group["lr"]
+            if group["use_muon"]:
+                pass
 
             else:
                 for p in group["params"]:
